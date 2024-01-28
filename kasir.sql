181f938969e658d81605bdcaf087fd928e244abc
Fauzan <fauzandwimulyono@gmail.com>
2024-01-28 11:19:39 +0700

    commit

---
+++ kasir.sql
@@ -0,0 +1,196 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 27, 2024 at 02:35 PM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.1.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_kasir`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `detailpenjualan`
+--
+
+CREATE TABLE `detailpenjualan` (
+  `DetailID` int(11) NOT NULL,
+  `PenjualanID` int(11) NOT NULL,
+  `ProdukID` int(11) NOT NULL,
+  `JumlahProduk` int(11) NOT NULL,
+  `SubTotal` decimal(10,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `detailpenjualan`
+--
+
+INSERT INTO `detailpenjualan` (`DetailID`, `PenjualanID`, `ProdukID`, `JumlahProduk`, `SubTotal`) VALUES
+(1, 1, 3, 2, '2000.00');
+
+--
+-- Triggers `detailpenjualan`
+--
+DELIMITER $$
+CREATE TRIGGER `kurangStock` AFTER INSERT ON `detailpenjualan` FOR EACH ROW UPDATE produk SET stock = stock - NEW.jumlahProduk
+WHERE ProdukID=NEW.ProdukID
+$$
+DELIMITER ;
+DELIMITER $$
+CREATE TRIGGER `nambahStock` AFTER DELETE ON `detailpenjualan` FOR EACH ROW UPDATE produk SET Stock = Stock + OLD.JumlahProduk
+WHERE ProdukID = OLD.ProdukID
+$$
+DELIMITER ;
+DELIMITER $$
+CREATE TRIGGER `nambahTotalHarga` AFTER INSERT ON `detailpenjualan` FOR EACH ROW UPDATE penjualan
+SET TotalHarga = TotalHarga + NEW.SubTotal
+WHERE PenjualanID=NEW.PenjualanID
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pelanggan`
+--
+
+CREATE TABLE `pelanggan` (
+  `PelangganID` int(11) NOT NULL,
+  `NamaPelanggan` varchar(255) NOT NULL,
+  `Alamat` text NOT NULL,
+  `NomorTelepon` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `pelanggan`
+--
+
+INSERT INTO `pelanggan` (`PelangganID`, `NamaPelanggan`, `Alamat`, `NomorTelepon`) VALUES
+(1, 'Oya Suryana', 'Bayuning', '0852241646'),
+(2, 'Abiq Sabiqul Khoir', 'Sukamulya', '08645454542');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `penjualan`
+--
+
+CREATE TABLE `penjualan` (
+  `PenjualanID` int(11) NOT NULL,
+  `TanggalPenjulaan` date NOT NULL,
+  `TotalHarga` decimal(10,2) NOT NULL,
+  `PelangganID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `penjualan`
+--
+
+INSERT INTO `penjualan` (`PenjualanID`, `TanggalPenjulaan`, `TotalHarga`, `PelangganID`) VALUES
+(1, '2024-01-26', '7000.00', 1);
+
+--
+-- Triggers `penjualan`
+--
+DELIMITER $$
+CREATE TRIGGER `mengurangitotalharga` AFTER DELETE ON `penjualan` FOR EACH ROW UPDATE penjualan SET TotalHarga = TotalHarga - OLD.TotalHarga
+WHERE PenjualanID = OLD.PenjualanID
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `produk`
+--
+
+CREATE TABLE `produk` (
+  `ProdukID` int(11) NOT NULL,
+  `NamaProduk` varchar(255) NOT NULL,
+  `Harga` decimal(10,2) NOT NULL,
+  `Stock` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `produk`
+--
+
+INSERT INTO `produk` (`ProdukID`, `NamaProduk`, `Harga`, `Stock`) VALUES
+(1, 'Indomie Rasa Ayam Bawang', '2500.00', 30),
+(2, 'Indomie Mie Goreng Mie Aceh', '2500.00', 17),
+(3, 'Teh Gelas', '1000.00', 8);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `detailpenjualan`
+--
+ALTER TABLE `detailpenjualan`
+  ADD PRIMARY KEY (`DetailID`);
+
+--
+-- Indexes for table `pelanggan`
+--
+ALTER TABLE `pelanggan`
+  ADD PRIMARY KEY (`PelangganID`);
+
+--
+-- Indexes for table `penjualan`
+--
+ALTER TABLE `penjualan`
+  ADD PRIMARY KEY (`PenjualanID`);
+
+--
+-- Indexes for table `produk`
+--
+ALTER TABLE `produk`
+  ADD PRIMARY KEY (`ProdukID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `detailpenjualan`
+--
+ALTER TABLE `detailpenjualan`
+  MODIFY `DetailID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `pelanggan`
+--
+ALTER TABLE `pelanggan`
+  MODIFY `PelangganID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `penjualan`
+--
+ALTER TABLE `penjualan`
+  MODIFY `PenjualanID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `produk`
+--
+ALTER TABLE `produk`
+  MODIFY `ProdukID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
