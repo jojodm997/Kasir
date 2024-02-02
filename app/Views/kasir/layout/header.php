c292b41206046179f8e95063c3336160f25bb3e4
Fauzan <fauzandwimulyono@gmail.com>
2024-02-02 08:15:22 +0700

    lol

--- app/Views/kasir/layout/header.php
+++ app/Views/kasir/layout/header.php
@@ -325,27 +325,6 @@
         id="navbarBlur"
         navbar-scroll="true"
       >
-        <div class="container-fluid py-1 px-2">
-          <nav aria-label="breadcrumb">
-            <ol
-              class="breadcrumb bg-transparent mb-1 pb-0 pt-1 px-0 me-sm-6 me-5"
-            >
-              <li class="breadcrumb-item text-sm">
-                <a class="opacity-5 text-dark" href="javascript:;">Example</a>
-              </li>
-              <li
-                class="breadcrumb-item text-sm text-dark active"
-                aria-current="page"
-              >
-                Example
-              </li>
-            </ol>
-            <h6 class="font-weight-bold mb-0">Example</h6>
-          </nav>
-          <div
-            class="collapse navbar-collapse mt-sm-0 mt-2 me-md-0 me-sm-4"
-            id="navbar"
-          >
             <div class="ms-md-auto pe-md-3 d-flex align-items-center">
               <div class="input-group">
                 <span class="input-group-text text-body bg-white border-end-0">
