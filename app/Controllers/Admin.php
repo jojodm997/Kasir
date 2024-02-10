c7dd0af7e4b96010610d7226da57df7a4126ebb1
Fauzan <fauzandwimulyono@gmail.com>
2024-02-10 19:08:22 +0700

    changes

--- app/Controllers/Admin.php
+++ app/Controllers/Admin.php
@@ -6,9 +6,21 @@ class Admin extends BaseController
 {
    public function index()
     {
+        $data ['title'] = 'Dashboard';
+        return view('admin/index', $data);
+    }
+
+    public function muser()
+    {
         $data ['title'] = 'User list';
         $users = new \Myth\Auth\Models\UserModel();
         $data['users'] = $users->findAll();
-        return view('admin/index', $data);
+        return view('admin/muser', $data);
+    }
+
+    public function laporan()
+    {
+        $data ['title'] = 'Laporan';
+        return view('admin/laporan', $data);
     }
 }
