d2ba6936cbb43791046cc9d76de881adf4fa5f09
Fauzan <fauzandwimulyono@gmail.com>
2024-02-10 15:10:18 +0700

    changes

--- app/Views/templates/sidebar.php
+++ app/Views/templates/sidebar.php
@@ -172,54 +172,6 @@
             </a>
           </li>
           <?php endif; ?>
-          <li class="nav-item">
-            <a class="nav-link" href="<?= base_url('kasir') ?>">
-              <div
-                class="icon icon-shape icon-sm px-0 text-center d-flex align-items-center justify-content-center"
-              >
-                <svg
-                  width="30px"
-                  height="30px"
-                  viewBox="0 0 48 48"
-                  version="1.1"
-                  xmlns="http://www.w3.org/2000/svg"
-                  xmlns:xlink="http://www.w3.org/1999/xlink"
-                >
-                  <title>rtl</title>
-                  <g
-                    id="rtl"
-                    stroke="none"
-                    stroke-width="1"
-                    fill="none"
-                    fill-rule="evenodd"
-                  >
-                    <g
-                      id="menu-alt-3"
-                      transform="translate(12.000000, 14.000000)"
-                      fill="#FFFFFF"
-                    >
-                      <path
-                        class="color-foreground"
-                        d="M0,1.71428571 C0,0.76752 0.76752,0 1.71428571,0 L22.2857143,0 C23.2325143,0 24,0.76752 24,1.71428571 C24,2.66105143 23.2325143,3.42857143 22.2857143,3.42857143 L1.71428571,3.42857143 C0.76752,3.42857143 0,2.66105143 0,1.71428571 Z"
-                        id="Path"
-                      ></path>
-                      <path
-                        class="color-background"
-                        d="M0,10.2857143 C0,9.33894857 0.76752,8.57142857 1.71428571,8.57142857 L22.2857143,8.57142857 C23.2325143,8.57142857 24,9.33894857 24,10.2857143 C24,11.2325143 23.2325143,12 22.2857143,12 L1.71428571,12 C0.76752,12 0,11.2325143 0,10.2857143 Z"
-                        id="Path"
-                      ></path>
-                      <path
-                        class="color-background"
-                        d="M10.2857143,18.8571429 C10.2857143,17.9103429 11.0532343,17.1428571 12,17.1428571 L22.2857143,17.1428571 C23.2325143,17.1428571 24,17.9103429 24,18.8571429 C24,19.8039429 23.2325143,20.5714286 22.2857143,20.5714286 L12,20.5714286 C11.0532343,20.5714286 10.2857143,19.8039429 10.2857143,18.8571429 Z"
-                        id="Path"
-                      ></path>
-                    </g>
-                  </g>
-                </svg>
-              </div>
-              <span class="nav-link-text ms-1">RTL</span>
-            </a>
-          </li>
           <li class="nav-item mt-2">
             <div class="d-flex align-items-center nav-link">
               <svg
