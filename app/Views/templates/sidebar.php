ed82d1d55046d568d2a2f1f7abd5e9dd39cd7791
Fauzan <fauzandwimulyono@gmail.com>
2024-02-14 18:58:53 +0700

    changes

--- app/Views/templates/sidebar.php
+++ app/Views/templates/sidebar.php
@@ -266,45 +266,6 @@
           </li>
         </ul>
       </div>
-      <div class="sidenav-footer mx-4">
-        <div class="card border-radius-md" id="sidenavCard">
-          <div class="card-body text-start p-3 w-100">
-            <div class="mb-3">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="16"
-                height="16"
-                class="text-primary"
-                viewBox="0 0 24 24"
-                fill="currentColor"
-                id="sidenavCardIcon"
-              >
-                <path
-                  d="M5.625 1.5c-1.036 0-1.875.84-1.875 1.875v17.25c0 1.035.84 1.875 1.875 1.875h12.75c1.035 0 1.875-.84 1.875-1.875V12.75A3.75 3.75 0 0016.5 9h-1.875a1.875 1.875 0 01-1.875-1.875V5.25A3.75 3.75 0 009 1.5H5.625z"
-                />
-                <path
-                  d="M12.971 1.816A5.23 5.23 0 0114.25 5.25v1.875c0 .207.168.375.375.375H16.5a5.23 5.23 0 013.434 1.279 9.768 9.768 0 00-6.963-6.963z"
-                />
-              </svg>
-            </div>
-            <div class="docs-info">
-              <h6 class="font-weight-bold up mb-2">Need help?</h6>
-              <p class="text-sm font-weight-normal">Please check our docs.</p>
-              <a
-                href="https://www.creative-tim.com/learning-lab/bootstrap/license/corporate-ui-dashboard"
-                target="_blank"
-                class="font-weight-bold text-sm mb-0 icon-move-right mt-auto w-100 mb-0"
-              >
-                Documentation
-                <i
-                  class="fas fa-arrow-right-long text-sm ms-1"
-                  aria-hidden="true"
-                ></i>
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
     </aside>
     <main
       class="main-content position-relative max-height-vh-100 h-100 border-radius-lg"
