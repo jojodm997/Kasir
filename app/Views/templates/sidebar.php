a2468a14ddcfc77dda6339c2e80b1438b5d770d4
Fauzan <fauzandwimulyono@gmail.com>
2024-02-19 17:33:40 +0700

    changes

--- app/Views/templates/sidebar.php
+++ app/Views/templates/sidebar.php
@@ -131,7 +131,7 @@
                   </g>
                 </svg>
               </div>
-              <span class="nav-link-text ms-1">Tables</span>
+              <span class="nav-link-text ms-1">User</span>
             </a>
           </li>
            <?php endif; ?>
